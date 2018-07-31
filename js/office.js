25db40bda435396f625cdfe50bbbd769d0409139
notte <maonome@gmail.com>
2018-07-31 16:27:59 +0800

    123
    123

---
+++ js/office.js
@@ -0,0 +1,71 @@
+$(function() {
+
+
+	// header欄位控制
+    $(window).scroll(function() {
+
+        console.log()
+
+        if ($('header').offset().top > 100) {
+
+            $('header').addClass('header-move');
+
+        } else {
+
+            $('header').removeClass('header-move');
+        }
+
+    })
+
+
+
+    // Unique事件開始
+
+    // icon消失，文字出現
+    $('.OfferIcon').mouseenter(function() {
+
+        $(this).siblings('.OfferItem').show();
+        $(this).hide();
+
+    });
+
+    // 文字消失，icon出現
+    $('.OfferItem').mouseleave(function() {
+
+        $(this).siblings('.OfferIcon').show();
+        $(this).hide();
+
+    });
+
+    // Unique事件結束
+
+
+    // header button滑動
+    $('.header-btn').click(function() {
+
+    	var i=$(this).attr('href').substr(1);
+
+    $('html,body').animate(
+
+        { scrollTop: $('#'+i).offset().top }
+
+        , 800
+
+
+    );
+
+    });
+
+    // 捲軸滑動
+
+    $(window).scroll(function(){
+
+    	
+
+    });
+
+
+
+
+
+});
