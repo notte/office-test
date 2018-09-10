db96e6e25632b636e11a44dba3211232ec512510
notte <maonome@gmail.com>
2018-09-10 16:14:45 +0800

    gfhfgh
    fghfght

---
+++ js/array.js
@@ -0,0 +1,8 @@
+var JobArray = [{
+        "Category": "", //1PM，2特殊，3UIUX，4平台，5行動，6系統
+        "Job": "", //職稱
+        "Amount": "", //工作內容
+        "Description": "", //其他條件
+        "email":"", //主管mail
+    },
+];
